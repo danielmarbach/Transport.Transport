08889368f8ac39e1dcac260b23a9803324c74303
danielmarbach <daniel.marbach@openplace.net>
2014-02-07 13:50:32 +0100

    Simple RabbitStart

---
+++ RabbitMQ2NServiceBus/src/ch/danielmarbach/rabbitmq2nservicebus/Earth.java
@@ -0,0 +1,10 @@
+package ch.danielmarbach.rabbitmq2nservicebus;
+
+public class Earth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("jjkkk");
+	}
+
+}
