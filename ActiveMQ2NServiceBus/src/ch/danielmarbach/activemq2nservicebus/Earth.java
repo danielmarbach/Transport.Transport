c216d6b20c51a3d575f80026da04fface2192ee7
danielmarbach <daniel.marbach@openplace.net>
2014-02-07 14:38:14 +0100

    Simple ActiveMQStart

---
+++ ActiveMQ2NServiceBus/src/ch/danielmarbach/activemq2nservicebus/Earth.java
@@ -0,0 +1,47 @@
+package ch.danielmarbach.activemq2nservicebus;
+
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Earth {
+
+	public static void main(String args[]) throws Throwable {
+		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+				"vm://localhost");
+
+		// Create a Connection
+		Connection connection = connectionFactory.createConnection();
+		connection.start();
+
+		// Create a Session
+		Session session = connection.createSession(false,
+				Session.AUTO_ACKNOWLEDGE);
+
+		// Create the destination (Topic or Queue)
+		Destination destination = session.createQueue("TEST.FOO");
+
+		// Create a MessageProducer from the Session to the Topic or Queue
+		MessageProducer producer = session.createProducer(destination);
+		producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+		// Create a messages
+		String text = "Hello world! From: " + Thread.currentThread().getName()
+				+ " : ";
+		TextMessage message = session.createTextMessage(text);
+
+		// Tell the producer to send the message
+		System.out.println("Sent message: " + message.hashCode() + " : "
+				+ Thread.currentThread().getName());
+		producer.send(message);
+
+		// Clean up
+		session.close();
+		connection.close();
+	}
+}
